5f764855ecc8956edf9800aaaa5a26ff892a7304
a-kali <2509660158@qq.com>
2021-07-19 20:44:58 +0800

    增加了发送邮件的功能

---
+++ mail.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+from setup import *
+
+
+def send_email(content, title):
+    message = MIMEText(content, 'plain', 'utf-8')
+    message['Subject'] = title
+    message['From'] = sender
+    message['To'] = receivers[0]
+
+    try:
+        smtpObj = smtplib.SMTP()
+        smtpObj.connect(mail_host, 25)
+        smtpObj = smtplib.SMTP_SSL(mail_host)
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        smtpObj.quit()
+    except smtplib.SMTPException as e:
+        print(e)
+        send_email(content, title)
---
+++ main.py
@@ -0,0 +1,13 @@
+
+#设置服务器所需信息
+#163邮箱服务器地址
+
+
+
+
+# try:
+#
+# except smtplib.SMTPException as e:
+#     print('error',e) #打印错误
+
+
